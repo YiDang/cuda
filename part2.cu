8c16e084fa10a8ed5c48f9ebda785900c4c8b2a7
YiDang <dangyi0811@hotmail.com>
2017-11-22 14:28:37 -0500

    a

---
+++ part2.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+const int N = 16; 
+const int blocksize = 16; 
+
+const int dimx = 10000;
+const int dimy = 10000;
+
+__global__ 
+void hello(char *a, int *b) 
+{
+	
+}
+ 
+int main()
+{
+	float[dimx][dimy];
+	printf("%d\n", float[1][1]);
+	return EXIT_SUCCESS;
+}
