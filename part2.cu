19349a9ace65e8e4c1e08d28f5851ec9fe3d976b
YiDang <dangyi0811@hotmail.com>
2017-11-28 19:27:50 -0500

    ???

--- part2.cu
+++ part2.cu
@@ -83,6 +83,7 @@ __global__ void Multiply(float *arrayA, float *arrayB, float *arrayC, unsigned i
 	    for(int i = 0; i < n; i++)
         {
 	    	sC[idx] += sA[row * n + i] * sB[i * p + col];
+            if(idx == 0 ) printf("sc = %2f , sa = %2f , sb = %2f \n", sC[idx], sA[row * n + i] , sB[i * p + col]);
 	    }
         arrayC[idx] = sC[idx];
     }
