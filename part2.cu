bb1fcec16159d2403193ce710268dcb3e04e5a22
YiDang <dangyi0811@hotmail.com>
2017-12-12 16:48:27 -0500

    f

--- part2.cu
+++ part2.cu
@@ -366,7 +366,7 @@ int main(int argc, char **argv)
 	float *host_array_C_texture = (float*)malloc(M_*P_*sizeof(float));
 	float *host_array_C_cublas = (float*)malloc(M_*P_*sizeof(float));
 
-    int showma = 1, showdif = 0;
+    int showma = 0, showdif = 0;
 	double diff = 0;
     cudaInit(host_array_A, M_, N_);
 	//show(host_array_A, M_, N_);
